14e6b636bc1f84a28a8ce4555015da113f44805a
Jagadeesh3996 <jagadeeshau032@gmail.com>
2025-07-12 21:57:14 +0530

    test7

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <?php include("./utilities/nav.php") ?>
     <!-- Navbar - End -->
 
-    <h1><?= $_SERVER['DOCUMENT_ROOT'] . '../admin/pdf/pdf.php' ?></h1>
-    
+    <h1><?= $_SERVER['DOCUMENT_ROOT'] . '/../newadmin/pdf/pdf.php' ?></h1>
+
     <!-- Hero Section - Start -->
     <div class="container-fluid hero homeHero">
         <div class="owl-carousel owl-theme homeHeroCaro">
